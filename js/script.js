c172775f00915e24b23040e160771b5dd90734a6
kausar-blog <kausarprogrammer@gmail.com>
2026-02-26 23:59:26 +0600

    js id selected

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Select elements
+const TotalBalanceAmount = document.getElementById("balanceAmount");
+const incomeTotal = document.getElementById("incomeTotal");
+const expensesTotal = document.getElementById("expensesTotal");
+const transactionDetails = document.getElementById("transactionDetails");
+const transactionAmount = document.getElementById("transactionAmount");
+const addBtn = document.getElementById("addBtn");
+const subtractBtn = document.getElementById("subtractBtn");
+const transactionContainer = document.getElementById("transactionContainer");
+
+// Initialize totals
+let balance = 0;
+let income = 0;
+let expenses = 0;
